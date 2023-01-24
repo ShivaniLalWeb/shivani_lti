42ba3a620dd43fbe1af4d2ff64957b2b9233a5ed
ShivaniLalWeb <shivani.lal@weboniselab.com>
2023-01-24 17:56:05 +0530

    our services section

---
+++ main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+$('.community_carousel').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    centerMode: true,
+    communityCarousel: true
+  })
+});
