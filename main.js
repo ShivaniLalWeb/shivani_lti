99e2fe2af2c4690ef8fce9cf71db71dc72eac3c5
ShivaniLalWeb <shivani.lal@weboniselab.com>
2023-01-25 16:40:30 +0530

    first commit

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 $(document).ready(function(){
 
-$('.community_carousel').slick({
+$('.variable-width').slick({
     dots: true,
+    prevArrow: '<i class="fa fa-arrow-left">',
+    nextArrow: '<i class="fa fa-arrow-right">',
     infinite: true,
     speed: 300,
-    slidesToShow: 1,
-    centerMode: true,
-    communityCarousel: true
+    slidesToShow: 3,
   })
+
+    $('.banner_carousel').slick({
+      dots: true,
+      arrows: false,
+      appendDots: $('.slick-slider-dots'),
+    });
+      
 });
